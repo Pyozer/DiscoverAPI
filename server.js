da81ba256abb33a9269ba8dc83a1236cc3e72950
Sloknatos <adam.louis28@gmail.com>
2019-04-30 11:26:31 +0200

    reforge API

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ let PORT = process.env.PORT || 3000;
 app.get('/hello', (req, res) => {
   return res.json({
     status: "succeed",
-    message: "Hello World !"
+    data: "Hello World !",
+    message: "201"
   });
 });
 
 app.get('/hello/:name', (req, res) => {
   return res.json({
     status: "succeed",
-    message: `Hello ${req.params.name}`
+    data: `Hello ${req.params.name}`
+    message: "201"
   });
 });
 
