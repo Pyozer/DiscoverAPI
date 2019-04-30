336211c5484b767512fe398f6c472d637a509a65
Sloknatos <adam.louis28@gmail.com>
2019-04-30 14:56:13 +0200

    test DB

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const app = express();
-let PORT = process.env.PORT || 3000;
-
-app.get('/hello', (req, res) => {
-  return res.json({
-    status: "succeed",
-    data: "Hello World !",
-    message: "200"
-  });
-});
-
-app.get('/hello/:name', (req, res) => {
-  return res.json({
-    status: "succeed",
-    data: `Hello ${req.params.name}`,
-    message: "200"
-  });
-});
-
-
-
-app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
-);
-
+// const express = require('express');
+// const app = express();
+// let PORT = process.env.PORT || 3000;
+//
+// app.get('/hello', (req, res) => {
+//   return res.json({
+//     status: "succeed",
+//     data: "Hello World !",
+//     message: "200"
+//   });
+// });
+//
+// app.get('/hello/:name', (req, res) => {
+//   return res.json({
+//     status: "succeed",
+//     data: `Hello ${req.params.name}`,
+//     message: "200"
+//   });
+// });
+//
+//
+//
+// app.listen(PORT, () =>
+//   console.log(`Example app listening on port ${PORT}!`),
+// );
 const http = require('http');
 const { Client } = require('pg');
 
+const PORT = process.env.PORT || 5000;
 const { DATABASE_URL } = process.env;
 const server = http.createServer((req, res) => {
   const client = new Client({
@@ -44,4 +44,8 @@ const server = http.createServer((req, res) => {
       res.end('ERROR');
       client.end();
     });
+});
+server.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
 });
