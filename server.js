35955e4c546707b39058a25ac011b6f4b4ac7c4d
Sloknatos <adam.louis28@gmail.com>
2019-04-30 14:45:38 +0200

    add postgre

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ client.connect();
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
+  console.log('-------');
+  console.log('-------');
+  console.log('-------');
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
