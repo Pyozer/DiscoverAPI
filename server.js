1000e35de1f61bb6260e76e5bf61c2d054dcbaf1
Sloknatos <adam.louis28@gmail.com>
2019-04-30 14:48:31 +0200

    test DB

--- server.js
+++ server.js
@@ -24,24 +24,25 @@ app.listen(PORT, () =>
   console.log(`Example app listening on port ${PORT}!`),
 );
 
-console.log(process.env);
-
+onst http = require('http');
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  console.log('-------');
-  console.log('-------');
-  console.log('-------');
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+const PORT = process.env.PORT || 5000;
+const { DATABASE_URL } = process.env;
+const server = http.createServer((req, res) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  client.connect()
+    .then(() => client.query('SELECT * FROM hellotable'))
+    .then((result) => {
+      res.end(`${result.rows[0].name}\n`);
+      client.end();
+    })
+    .catch(() => {
+      res.end('ERROR');
+      client.end();
+    });
 });
