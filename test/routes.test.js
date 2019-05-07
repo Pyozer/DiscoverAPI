4cca1577052ad45a1d6c07118d628cd9c2b171a6
Dylan DE SOUSA <20180206@B15-05.local>
2019-05-07 15:35:53 +0200

    Adding test to check if /api route is ok

---
+++ test/routes.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('App',() => {
+  describe('End Points', () => {
+    describe('GET /api', () => {
+      test('It should response the GET method', () => {
+        return request(app).get('/api').expect(200)
+      })
+
+      test("It should return a JSON : {'message': 'API Discover'}", async () => {
+        const response = await request(app).get('/api')
+        const expectedMessage = {'message': 'API Discover'}
+
+        expect(response.body).toEqual(expectedMessage)
+      })
+    })
+  })
+})
