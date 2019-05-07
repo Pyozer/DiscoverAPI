2121f5ef664c8c5f5c14692d56edd74f06063208
Dylan DE SOUSA <20180206@B15-05.local>
2019-05-07 11:52:12 +0200

    Forgotten comma

--- config/database.test.config.js
+++ config/database.test.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "host": process.env.DB_TEST_URL
+  "host": process.env.DB_TEST_URL,
   "user": process.env.DB_TEST_USER,
   "password": process.env.DB_TEST_PWD,
   "database": process.env.DB_TEST_DB,
