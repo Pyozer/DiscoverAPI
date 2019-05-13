7775e987495cedb0a0f3063c1030deacc5e8c204
Sloknatos <adam.louis28@gmail.com>
2019-05-13 09:16:09 +0200

    autocommit

--- server.js
+++ server.js
@@ -158,7 +158,11 @@ app.use('/api', router);
 
 var api_key = process.env.MAILGUN_API_KEY;
 var domain = process.env.MAILGUN_DOMAIN;
+
+console.log('hello world');
+
 console.log(`apikey: ${api_key}, domain: ${domain}`);
+
 var mailgun = require('mailgun-js')({
   apiKey: api_key,
   domain: domain
