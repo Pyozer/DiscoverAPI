c802b1f42f6869b4568135aeecdbf46586654bc9
Dylan DE SOUSA <dylan@needelp.com>
2019-05-14 08:56:22 +0200

    Changing test messages

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,59 +2,71 @@ const request = require('supertest')
 const app = require('../app')
 
 describe("APP Routes", () => {
-	describe("Route[GET] /api", () => {
-		test('It should return statusCode 200' , async () => {
-			const response = await request(app).get('/api').set({"Accept-Language": "fr-FR"})
+	describe("/api", () => {
+		describe("Method GET", () => {
+			test('It should return statusCode 200' , async () => {
+				const response = await request(app).get('/api').set({"Accept-Language": "fr-FR"})
 
-			expect(response.statusCode).toBe(200)
-		})
+				expect(response.statusCode).toBe(200)
+			})
 
-		test('It should return a json: {"message": "API Discover"}', async () => {
-			const response = await request(app).get('/api').set({"Accept-Language": "fr-FR"})
+			test('It should return a json: {"message": "API Discover"}', async () => {
+				const response = await request(app).get('/api').set({"Accept-Language": "fr-FR"})
 
-			expect(response.body).toEqual({"message": "API Discover"})
-		})
-	})
-
-	describe("Route[GET] /api/tags", () => {
-		test('It should return statusCode 200', async () => {
-			const response = await request(app).get('/api/tags').set({"Accept-Language": "fr-FR"})
-
-			expect(response.statusCode).toBe(200)
+				expect(response.body).toEqual({"message": "API Discover"})
+			})
 		})
 	})
 
-	describe("Route[POST] /api/users", () => {
-		test("It should return statusCode 200", async () => {
-			const response = await request(app).post('/api/users').set({"Accept-Language": "fr-FR"})
+	describe("/api/tags", () => {
+		describe("Method GET", () => {
+			test('It should return statusCode 200', async () => {
+				const response = await request(app).get('/api/tags').set({"Accept-Language": "fr-FR"})
 
-			expect(response.statusCode).toBe(200)
+				expect(response.statusCode).toBe(200)
+			})
 		})
 	})
 
-	describe("Route[POST] /api/users/login", () => {
-		test("It should return statusCode 200", async () => {
-			const response = await request(app).post('/api/users/login').set({"Accept-Language": "fr-FR"})
+	describe("/api/users", () => {
+		describe("Method POST", () => {
+			test("It should return statusCode 200", async () => {
+				const response = await request(app).post('/api/users').set({"Accept-Language": "fr-FR"})
 
-			expect(response.statusCode).toBe(200)
+				expect(response.statusCode).toBe(200)
+			})
 		})
 	})
 
-	describe("Route[PUT] /api/users/:id_user/logout", () => {
-		test("It should return a json error saying you are not allowed to use this route", async () => {
-			const response = await request(app).put('/api/users/1/login').set({"Accept-Language": "fr-FR"})
-			const expectedResult = { "status": "error", "message": "Utilisateur non autorisé" }
+	describe("/api/users/login", () => {
+		describe("Method POST", () => {
+			test("It should return statusCode 200", async () => {
+				const response = await request(app).post('/api/users/login').set({"Accept-Language": "fr-FR"})
 
-			expect(response.body).toEqual(expectedResult)
+				expect(response.statusCode).toBe(200)
+			})
 		})
 	})
 
-	describe("Route[PUT] /api/users/:id_user/info", () => {
-		test("It should return a json error saying you are not allowed to use this route", async () => {
-			const response = await request(app).put('/api/users/1/logout').set({"Accept-Language": "fr-FR"})
-			const expectedResult = { "status": "error", "message": "Utilisateur non autorisé" }
+	describe("/api/users/:id_user/logout", () => {
+		describe("Method PUT", () => {
+			test("It should return a json error saying you are not allowed to use this route", async () => {
+				const response = await request(app).put('/api/users/1/logout').set({"Accept-Language": "fr-FR"})
+				const expectedResult = { "status": "error", "message": "Utilisateur non autorisé" }
 
-			expect(response.body).toEqual(expectedResult)
+				expect(response.body).toEqual(expectedResult)
+			})
+		})
+	})
+
+	describe("/api/users/:id_user/info", () => {
+		describe("Method GET", () => {
+			test("It should return a json error saying you are not allowed to use this route", async () => {
+				const response = await request(app).get('/api/users/1/info').set({"Accept-Language": "fr-FR"})
+				const expectedResult = { "status": "error", "message": "Utilisateur non autorisé" }
+
+				expect(response.body).toEqual(expectedResult)
+			})
 		})
 	})
 })
